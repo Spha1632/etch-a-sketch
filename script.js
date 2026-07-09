a97e557c8f4f84957c158eb9e1611bb7a0116114
Spha1632 <sphamandlan1632@gmail.com>
2026-07-09 14:22:42 +0200

    Wrote code to handle button click to generate new grid

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const divContainer = document.querySelector(".container");
-
 function generateGrid(size) {
+    const divContainer = document.querySelector(".container");
+    divContainer.innerHTML = "";
+
     for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
         column.classList.add("gridColumn")
@@ -10,27 +11,42 @@ function generateGrid(size) {
             const gridBox = document.createElement("div");
             gridBox.classList.add("gridBox")
             column.append(gridBox)
-            gridBox.style.height = `${500/size}px`
-            gridBox.style.width = `${500/size}px`
+            gridBox.style.height = `${500 / size}px`
+            gridBox.style.width = `${500 / size}px`
         }
     }
 }
 
 function handleHover() {
     const gridBoxes = document.querySelectorAll(".gridBox");
+    const gridColor = document.querySelector(".gridColor");
 
     gridBoxes.forEach((gridBox) => {
-        
         gridBox.addEventListener("mouseover", () => {
-            gridBox.style["background-color"] = "red";
+            gridBox.style["background-color"] = gridColor.value;
+
         })
     })
-    
+
 }
 
+function handleButtonClick() {
+    const button = document.querySelector(".gridSize");
+
+    button.addEventListener("click", () => {
+        let gridSize;
+        do {
+            gridSize = parseInt(prompt("Enter Grid size"));
+        } while (gridSize < 8 || gridSize > 100)
+        
+        generateGrid(gridSize)
+        handleHover();
+    })
+}
 
 generateGrid(16);
 handleHover();
+handleButtonClick();
 
 
 
